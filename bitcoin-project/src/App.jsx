ea613098769f47eeb4db4f882731e1b01be68a5b
Sean <seanselbyy@gmail.com>
2024-12-12 11:28:32 +1300

    Add fetch

--- bitcoin-project/src/App.jsx
+++ bitcoin-project/src/App.jsx
@@ -4,6 +4,15 @@ const App = () => {
 
   const price = '100,000';
 
+  const [value, setValue] = useState();
+
+  const response = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json");
+
+  if (response.ok) {
+    let data = await response.json();
+    setValue(data);
+  }
+
   return (
     <div>
       <h1>Bitcoin Price</h1>
